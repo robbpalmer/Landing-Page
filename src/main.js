3a7666a40e2d616b4d880a0fd930dfc640f3888c
robbpalmer <72511048+robbpalmer@users.noreply.github.com>
2022-01-25 12:56:10 +0000

    added animation to show clickability of fakementum logo

--- src/main.js
+++ src/main.js
@@ -13,12 +13,3 @@ window.addEventListener('scroll', () => {
     updateList();
 })
 
-function logSubmit(event) {
-    console.log(`Form Submitted! Time stamp: ${event.timeStamp}`);
-    event.preventDefault();
-}
-  
-  const form = document.getElementById('form');
-  const log = document.getElementById('log');
-  form.addEventListener('submit', logSubmit);
-  
